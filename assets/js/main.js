8ef146500921094fe6c1db30dcc9ce41c5e431b3
Wallace Pinto <wall_h@hotmail.com>
2020-05-06 17:32:46 -0300

    Arrumando Alunos com Carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -143,12 +143,30 @@
         // Ou
         //$('#idDaDiv').show();
     });
-})(jQuery);
 
-// Para exibir o box flutuante
-$('#idDoBotao').on('click', function(e) {
-    e.preventdefault();
-    $('#idDaDiv').fadeIn();
-    // Ou
-    //$('#idDaDiv').show();
-});
+    $(document).ready(function() {
+        $("#myCarousel").on("slide.bs.carousel", function(e) {
+            var $e = $(e.relatedTarget);
+            var idx = $e.index();
+            var itemsPerSlide = 3;
+            var totalItems = $(".carousel-item").length;
+
+            if (idx >= totalItems - (itemsPerSlide - 1)) {
+                var it = itemsPerSlide - (totalItems - idx);
+                for (var i = 0; i < it; i++) {
+                    // append slides to end
+                    if (e.direction == "left") {
+                        $(".carousel-item")
+                            .eq(i)
+                            .appendTo(".carousel-inner");
+                    } else {
+                        $(".carousel-item")
+                            .eq(0)
+                            .appendTo($(this).find(".carousel-inner"));
+                    }
+                }
+            }
+        });
+    });
+
+})(jQuery);
